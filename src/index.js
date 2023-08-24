769f41f4fef68bc574f11ade91df5f3302469bae
Dan Leatherman <dleatherman@gmail.com>
2023-08-24 18:47:15 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './style';
+import Clock from './components/Clock';
+
+import SettingsIcon from './icons/settings';
+
+export default function App() {
+	return (
+		<div>
+			<SettingsIcon />
+			<Clock />
+		</div>
+	);
+}
